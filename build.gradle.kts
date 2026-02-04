82c756dd8406b7bc276ad0a6f18bb5a248277732
LuisDaMich <luisda.michel@gmail.com>
2026-02-03 21:34:38 -0600

    [LDMP][TESTING] create unit test for PackageService

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,12 +27,19 @@ dependencies {
     implementation("org.postgresql:postgresql:$postgres_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:1.5.9")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
     implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     implementation("org.jetbrains.exposed:exposed-core:0.50.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.50.0")
     implementation("org.jetbrains.exposed:exposed-dao:0.50.0")
     implementation ("org.jetbrains.exposed:exposed-java-time:0.50.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("com.h2database:h2:2.2.224")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
