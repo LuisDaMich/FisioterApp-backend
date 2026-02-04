1dd345f44262971de513f70e9812656fe5ce48ce
LuisDaMich <luisda.michel@gmail.com>
2026-02-03 19:02:26 -0600

    [LDMP][REFACTOR] rename routing class

--- src/main/kotlin/PatientRouting.kt
+++ src/main/kotlin/PatientRouting.kt
@@ -17,7 +17,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-fun Application.configureRouting() {
+fun Application.patientRouting() {
     routing {
         get("/") {
             call.respondText("FisioterApp Server, up and running")
